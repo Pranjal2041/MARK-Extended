1123018770624d24ef75e50f524b7f24a5a0e120
Pranjal Aggarwal <pranjal2041@gmail.com>
2022-04-05 19:23:08 +0530

    Add MarketCL Code

---
+++ experiments/MarketExperiment2/scripts/utils.py
@@ -0,0 +1,57 @@
+import torch
+import os
+from os.path import join
+import shutil
+
+def list_to_device(lis, device : str): return [x.to(device) for x in lis]
+
+def get_num_params_in_model(model : torch.nn.Module):  return sum(p.numel() for p in model.parameters())
+
+class AverageMeter:
+    """Computes and stores the average and current value"""
+    def __init__(self): self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+
+def get_acc(logits, labels):
+    return sum(logits.argmax(dim=1) == labels) / logits.shape[0]
+
+def create_backup(folders = None, files = None, backup_dir = 'experiments'):
+    if folders is None:
+        folders = ['.']
+    if files is None:
+        files = ['.py', '.txt', '.json','.cfg']
+
+    for folder in folders:
+        if not os.path.isdir(folder):
+            continue
+        for file in os.listdir(folder):
+            if file.endswith(tuple(files)):
+                if folder != '.':
+                    src = join(folder, file)
+                    dest = join(backup_dir, folder, file)
+                else:
+                    src = file
+                    dest = join(backup_dir, file)
+                os.makedirs(os.path.split(dest)[0], exist_ok=True)
+                shutil.copy(src, dest)
+
+
+def lstm_loss(criterions, logits, labels):
+    return sum([criterions[i](logits[i], labels[:,i]) for i in range(labels.shape[1])])/logits[0].shape[0]
+
+def lstm_accuracy(logits, labels):
+    return {i:(torch.max(logits[i], 1)[1] == labels[:, i]).float().mean().item() for i in range(labels.shape[1])}
+
+def lstm_f1(metric, logits, labels):
+    {i:metric[i](logits[i].cpu(), labels[:,i].cpu()) for i in range(len(logits))}
