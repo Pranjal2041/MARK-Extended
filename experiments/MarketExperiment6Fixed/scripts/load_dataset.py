cca77438f22bda6cd9baacf9f92eb429185f76f0
Pranjal Aggarwal <pranjal2041@gmail.com>
2022-04-13 23:42:59 +0530

    Clean Code

--- experiments/MarketExperiment6Fixed/scripts/load_dataset.py
+++
@@ -1,7 +0,0 @@
-# Sample file on how to load the datasets
-
-import pickle
-from data_utils import ImageGridData, ImageData, MyDS
-
-dset_grid = pickle.load(open('datasets/image_grid_data.pickle', 'rb'))
-dset_image = pickle.load(open('datasets/image_data.pickle', 'rb'))
